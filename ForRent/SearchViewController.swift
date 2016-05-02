5a19f3287591c8700718bd9091d515bdc753454a
Jianxin Gao <chienshin1@gmail.com>
2016-05-02 13:22:22 -0700

    completed location selecting function

--- ForRent/SearchViewController.swift
+++ ForRent/SearchViewController.swift
@@ -12,15 +12,59 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var historyLocationList: [LocationItem] {
+        get {
+            if let locationDataList = NSUserDefaults.standardUserDefaults().arrayForKey("HistoryLocationList") as? [NSData] {
+                // Decode NSData into LocationItem object.
+                return locationDataList.map({ NSKeyedUnarchiver.unarchiveObjectWithData($0) as! LocationItem })
+            } else {
+                return []
+            }
+        }
+        set {
+            // Encode LocationItem object.
+            let locationDataList = newValue.map({ NSKeyedArchiver.archivedDataWithRootObject($0) })
+            NSUserDefaults.standardUserDefaults().setObject(locationDataList, forKey: "HistoryLocationList")
+        }
+    }
+    
     @IBAction func toggleSideMenuButtonClicked(sender: AnyObject) {
         toggleSideMenuView()
     }
     
     @IBAction func locationButtonClicked(sender: AnyObject) {
-        let customLocationPicker = LocationPicker()
-        let navigationController = UINavigationController(rootViewController: customLocationPicker)
-        presentViewController(navigationController, animated: true, completion: nil)
+        let locationPicker = LocationPicker()
+        locationPicker.addButtons()
+        locationPicker.alternativeLocations = historyLocationList.reverse()
+        locationPicker.alternativeLocationEditable = true
         
+        // Completion closures
+        locationPicker.selectCompletion = { selectedLocationItem in
+            print("Select completion closure: " + selectedLocationItem.name)
+        }
+        locationPicker.pickCompletion = { pickedLocationItem in
+            self.showLocation(pickedLocationItem)
+            self.storeLocation(pickedLocationItem)
+        }
+        locationPicker.deleteCompletion = { locationItem in
+            self.historyLocationList.removeAtIndex(self.historyLocationList.indexOf(locationItem)!)
+        }
+        
+        let navigationController = UINavigationController(rootViewController: locationPicker)
+        presentViewController(navigationController, animated: true, completion: nil)
+    }
+    
+    
+    func showLocation(locationItem: LocationItem) {
+        //locationNameTextField.text = locationItem.name
+        //locationAddressTextField.text = locationItem.formattedAddressString
+    }
+    
+    func storeLocation(locationItem: LocationItem) {
+        if let index = historyLocationList.indexOf(locationItem) {
+            historyLocationList.removeAtIndex(index)
+        }
+        historyLocationList.append(locationItem)
     }
 
 }
